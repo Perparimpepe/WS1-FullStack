a33a226cd4e361f33a42af49744be16adc385d8d
Perparimpepe <perparimpepe@gmail.com>
2025-09-26 17:52:06 +0300

    .

--- Workshop-2/Practices_1/directories.js
+++ Workshop-2/Practices_1/directories.js
@@ -3,18 +3,18 @@ const path = require('path');
 
 const folderName = 'newdata';
 
-fs.mkdirSync(folderName);
-console.log('Folder has been created:', folderName);
+//fs.mkdirSync(folderName);
+//console.log('Folder has been created:', folderName);
 
 
-const data1 = fs.readFileSync('example.txt');
-const data2 = fs.readFileSync('example2.txt');
+//const data1 = fs.readFileSync('example.txt');
+//const data2 = fs.readFileSync('example2.txt');
 
-const combined = data1 + '\n' + data2;
+//const combined = data1 + '\n' + data2;
 
-fs.writeFileSync(path.join(folderName, 'written.txt'), combined, 'utf8');
-console.log('Tiedosto kirjoitettu kansioon', folderName);
+//fs.writeFileSync(path.join(folderName, 'written.txt'), combined, 'utf8');
+//console.log('Tiedosto kirjoitettu kansioon', folderName);
 
 
-//fs.rmdirSync(folderName);
-//console.log('Folder Removed:', folderName);
+fs.rmdirSync(folderName);
+console.log('Folder Removed:', folderName);
