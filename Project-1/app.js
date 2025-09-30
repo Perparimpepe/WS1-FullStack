847387d765efe705fd21b71f54848ddcc8d53228
Perparimpepe <perparimpepe@gmail.com>
2025-09-30 16:19:00 +0300

    aloitettu projekti-1

--- Project-1/app.js
+++ Project-1/app.js
@@ -2,11 +2,56 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the homepage!');
+  res.sendFile(__dirname + '/frontpage.html');
 });
 
 app.get('/guestbook', (req, res) => {
-  res.send('Guestbook page');
+  fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error loading guestbook data');
+      return;
+    }
+    const messages = JSON.parse(data);
+    let html = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Guestbook</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+      </head>
+      <body>
+        <h1 class="m-3">Guestbook</h1>
+        <table class="table table-striped m-3">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Country</th>
+              <th>Date</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+    `;
+    messages.forEach(msg => {
+      html += `
+        <tr>
+          <td>${msg.id}</td>
+          <td>${msg.username}</td>
+          <td>${msg.country}</td>
+          <td>${msg.date}</td>
+          <td>${msg.message}</td>
+        </tr>
+      `;
+    });
+    html += `
+          </tbody>
+        </table>
+      </body>
+      </html>
+    `;
+    res.send(html);
+  });
 });
 
 app.get('/newmessage', (req, res) => {
