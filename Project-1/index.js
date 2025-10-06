9355948d190e13f5639646f7622c8f5810c697d3
Perparimpepe <perparimpepe@gmail.com>
2025-10-06 15:00:46 +0300

    Saatu 2 ensimmäistä sivua valmiiksi

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,63 +1,26 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-
-app.set("views", __dirname);
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname));
-
 const fs = require('fs');
 const path = require('path');
 
+// Middleware
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // <-- korjattu
+app.use(express.static(path.join(__dirname, 'public'))); // <-- staattiset tiedostot kuten CSS
+
+// Routes
 app.get('/', (req, res) => {
-  res.render("index");
+  res.render('index');
 });
 
 app.get('/guestbook', (req, res) => {
   fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Error loading guestbook data');
-      return;
+      return res.status(500).send('Error loading guestbook data');
     }
     const messages = JSON.parse(data);
-      let html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Guestbook</title>
-      </head>
-      <body>
-        <h1 class="m-3">Guestbook</h1>
-        <table border="1">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Username</th>
-              <th>Country</th>
-              <th>Date</th>
-              <th>Message</th>
-            </tr>
-          </thead>
-          <tbody>
-    `;
-    messages.forEach(msg => {
-      html += `
-        <tr>
-          <td>${msg.id}</td>
-          <td>${msg.username}</td>
-          <td>${msg.country}</td>
-          <td>${msg.date}</td>
-          <td>${msg.message}</td>
-        </tr>
-      `;
-    });
-    html += `
-          </tbody>
-        </table>
-      </body>
-      </html>
-    `;
-    res.send(html);
+    res.render('guestbook', { messages });
   });
 });
 
@@ -69,6 +32,7 @@ app.get('/ajaxmessage', (req, res) => {
   res.send('Ajax message page');
 });
 
+// Käynnistys
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
